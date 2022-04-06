26b2093d2af9e4f9a5206e43db9a77d00930c81d
JavaScriptJacksn <jbjackson247@gmail.com>
2022-04-06 18:02:32 +0000

    Function to dispaly pre-game introductory text and ask for a name

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def begin_game():
+    """
+    Begins new game, prints beginning message and asks for user name input
+    """
+
+    print("""
+    ------------------------------------\n
+    TACTICAL SIMULATION TRAINER\n
+    TOP SECRET\n
+    ------------------------------------\n
+    Welcome, operative.\n
+    Our great organisation is under a great threat,\n
+    with enemy ships approaching our\n
+    vessels positioned in the pacific ocean.\n
+    As you know, they guard a hidden treasure… The Lost City of Atlantis.\n
+    It is your mission to practise and perfect tactical ship placement\n
+    and offensive manuevers in this cutting-edge simulation.\n
+    Our livelyhoods rest on your shoulders.\n
+    Good Luck.
+    ------------------------------------
+    """)
+
+    name = input("To begin, enter your operative ID (name):\n")
+
+    print(f"Operative {name}, your training begins.")
+
+
+begin_game()
