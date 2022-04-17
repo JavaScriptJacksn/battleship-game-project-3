75c6cdfd2c59cc56de8a1f585d31f2b0b4293bc7
JavaScriptJacksn <jbjackson247@gmail.com>
2022-04-17 16:15:15 +0000

    Tweaks to game instructions and spelling changes

--- run.py
+++ run.py
@@ -6,26 +6,25 @@ def begin_game():
     Begins new game, prints beginning message and asks for user name input
     """
 
-    print("""
+    name = input("To begin, enter your operative ID (name):\n")
+    print(f"""
 --------------------------------------------------------------------------------\n
 TACTICAL SIMULATION TRAINER\n
 TOP SECRET\n
 --------------------------------------------------------------------------------\n
-Welcome, operative.\n
+Welcome, operative{name}.\n
 Our great organisation is under a great threat,\n
 with enemy ships approaching our\n
 vessels positioned in the pacific ocean.\n
 As you know, they guard a hidden treasure… The Lost City of Atlantis.\n
-It is your mission to practise and perfect tactical ship placement\n
+It is your mission to practise and perfect tactical ship placement
 and offensive manuevers in this cutting-edge simulation.\n
 Our livelyhoods rest on your shoulders.\n
 Note: Games last until the final ship is hit, game length/difficulty
-scales with the size of board.
+scales with the size of board.\n
 Good Luck.
 --------------------------------------------------------------------------------
 """)
-
-    name = input("To begin, enter your operative ID (name):\n")
 
     print(f"\nOperative {name}, your training begins.")
 
